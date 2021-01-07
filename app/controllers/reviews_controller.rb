515fb66c6c3fe16c34fc1ed9d21b9b29b10e4044
cengizcmataraci <cengizcmataraci@gmail.com>
2021-01-07 15:29:04 +0300

    Add review process

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,28 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
   end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to reviews_index_path, notice: 'success'
+    else
+      render :new, notice: 'error'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 10 }
+  belongs_to :user
 end
