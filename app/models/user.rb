dd536d941643d67a05ff71bcc08480fff6be73ba
cengizcmataraci <cengizcmataraci@gmail.com>
2021-01-16 00:08:40 +0300

    Try user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   attr_writer :login
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   validate :validate_username
@@ -9,6 +10,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews
+  # has_many :comments
 
   def login
     @login || self.username || self.email
@@ -16,8 +18,8 @@ class User < ApplicationRecord
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    if (login = conditions.delete(:login))
+      where(conditions).where(['lower(username) = :value OR lower(email) = :value', { :value => login.downcase }]).first
     else
       if conditions[:username].nil?
         where(conditions).first
